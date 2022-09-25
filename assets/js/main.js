3f82444fb20115234d75eb37de5240b774ebdd31
Nicolas <nicolassoares.santos@outlook.com>
2022-09-24 21:59:38 -0300

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+function getJSON(url, callback){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+  xhr.onload = function (){
+    if (xhr.status === 200){
+      console.log('Dados Rebidos com sucesso!')
+      callback(xhr.response);
+    }
+    else {
+      console.log('Problema ao conectar com a API: ' + xhr.status);
+    }
+
+    xhr.send();
+  }
+
+  var url = "https://api.covid19api.com/dayone/country/brazil";
+
+  getJSON(url, function (data){
+
+  })
+
+  getJSON(url, function(data){
+    for(var i=0; i<data.length; i++){
+      console.log(data[i]);
+    }
+  })
+}
