4f3ae653c3131af314173700752e067717b92936
Eve <evelinheks@gmail.com>
2022-10-05 10:51:43 -0300

    Dados no HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
+let saida;
 let url = 'https://api.covid19api.com/dayone/country/brazil';
+let html = document.getElementById("descricao");
 
-function getJSON(url, callback) {
+function getJSON(url) {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.responseType = 'json';
   xhr.onload = function () {
     if (xhr.status >= 200) {
-      callback(xhr.response);
+      retornaDados(xhr.response);
     } else {
       console.log("erro:" + xhr.status)
     }
@@ -14,8 +16,14 @@ function getJSON(url, callback) {
   xhr.send();
 }
 
+let cartoes = []
 function retornaDados(data) {
-  console.log(data[0]);
+  data.forEach(
+    element => {
+      saida = "Casos ativos:" + element.Active + "<BR>Mortes:" + element.Deaths + "<BR>Casos confirmados:" + element.Confirmed;
+      cartoes.push(saida + "<BR>")
+    });
+  html.innerHTML = cartoes;
 }
 
-getJSON(url, retornaDados);
+getJSON(url, retornaDados); //puxar com um botão
